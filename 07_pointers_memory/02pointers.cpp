2e8849016bcc9812caea19897e7daf82830358e3
syncdecodes <syncdecodes@gmail.com>
2026-05-15 17:05:03 +0530

    Updated pointers

--- 07_pointers_memory/02pointers.cpp
+++ 07_pointers_memory/02pointers.cpp
@@ -1,25 +1,84 @@
 #include <iostream>
 using namespace std;
-// pointers
 
-/*
-Variables that stores a memory address of another variable
-& address-of operator
-* dereference operator
-*/
+
+// Pointers * -:
 
 int main()
 {
-    std::string name = "Admin";
-    std::string *pName = &name;
+    int x = 10;
+    int *ptr = &x;
 
-    std::string freeFoods[5] = {"burger1", "burger2", "burger3", "burger4", "burger5"};
-    /* array is already a memory address */
+    string name = "Admin";
 
-    std::cout << pName << endl;  // address where var name is stored
-    std::cout << *pName << endl; // * to access value stored at the address within the pointer
-    std ::cout << freeFoods << endl;
-    std ::cout << *freeFoods << endl; // burger1
+    cout << "Address of var x: " << &x << endl;
+    cout << "Address of var x: " << ptr << endl << endl;
+    
+    cout << "value var x: " << x << endl;
+    cout << "value of var x: " << *ptr << endl << endl;
+
+    *ptr = 100;
+
+    cout << "Updated value of x: " << x << endl;
+    cout << "Updated value of x: " << *ptr << endl << endl;
+
+    cout << "Address of Admin: " << &name << endl;
+    cout << "Address of A of Admin: " << (void*)&name[0] << endl;
+    cout << "Address of d of Admin: " << (void*)&name[1] << endl;
+    cout << "Address of m of Admin: " << (void*)&name[2] << endl;
+    cout << "Address of i of Admin: " << (void*)&name[3] << endl;
+    cout << "Address of n of Admin: " << (void*)&name[4] << endl;
 
     return 0;
 }
+
+
+
+/*
+References & -:
+
+int main()
+{
+    int x = 10;
+    int &r = x; // Now r is another name for x
+
+    cout << &r;
+    return 0;
+}
+*/
+
+
+/*
+Function Parameters with pointers -:
+
+void change(int *p)
+{
+    *p = 100;
+}
+
+int main()
+{
+    int x = 10;
+    change(&x);
+    cout << x; // 100
+    return 0;
+}
+*/
+
+
+/*
+Function Parameters with References -:
+
+void change(int &x)
+{
+    x = 100;
+}
+
+int main()
+{
+    int x = 5;
+    change(x);
+    cout << x; // 100
+    return 0;
+}
+*/
