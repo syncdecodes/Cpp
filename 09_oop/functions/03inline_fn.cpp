141a0fda97aae7668a4853ae6203c9cd3d13c203
syncdecodes <syncdecodes@gmail.com>
2026-05-17 16:57:39 +0530

    Modify oop inline function

--- 09_oop/functions/03inline_fn.cpp
+++ 09_oop/functions/03inline_fn.cpp
@@ -14,3 +14,19 @@ int main()
     cout << sum (5, 10);
     return 0;
 }
+
+/*
+Instead of creating a seperate fn call like:
+sum(5, 10);
+The compiler may replace it with:
+5 + 10
+
+Advantages
+Reduces function call overhead
+Can improve performance for small, frequently used functions
+
+Notes
+The inline keyword is only a request to the compiler, not a command.
+Large or complex functions are usually not inlined.
+Functions defined inside a class are automatically treated as inline.
+*/
